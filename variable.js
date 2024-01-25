a93471389c1b72de9fe5e674db2b07ebc4ec0315
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-01-25 21:54:49 +0600

    Variable File Compleate

---
+++ variable.js
@@ -0,0 +1,21 @@
+/**
+ * Variable ==> Vari + Able
+ * 5 Things to declare a variable
+ *
+ * * */
+
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2005;
+
+var age;
+
+console.log(age);
+console.log(price);
+
+var age = 17;
+
+age = 35
+
+console.log(age)
