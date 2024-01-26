282a59b1564c6420fb5029cfdf2f22dd4f097a6d
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-01-26 08:59:54 +0600

    Operation File Compleate

---
+++ operation.js
@@ -0,0 +1,27 @@
+var eggPrice = 10;
+var onionPrice = 20;
+
+// console.log(eggPrice + onionPrice);
+// console.log(onionPrice - eggPrice);
+
+// console.log(onionPrice * eggPrice)
+// console.log(onionPrice / eggPrice)
+
+// Remainder
+console.log(10 % 3); // what will be the remainder after divideing 10 by 3
+console.log(11 % 7);
+
+// --------------------------------------------------------
+var money = 550;
+money = 500;
+
+var price = 35;
+price = price + 10;
+
+console.log(price);
+
+var newPrice = price + 50;
+
+
+var dsicountedPrice = newPrice - 10;
+console.log(dsicountedPrice)
