2178880d082ad63bf596d569bd2cf1a69784e4de
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-01-26 08:22:39 +0600

    Naming File Compleate

---
+++ naming.js
@@ -0,0 +1,31 @@
+// 1. no Keywird in variable name
+var false = 45
+var var = 50
+
+// 2. no Sace ir gap in variables name
+var is happy = false;
+var ishappy = true;
+
+// 3. no quote
+var "address" = "Kochu Khat";
+
+// 4. can not start with a number. but number other then the first letter is allowed
+
+var money = 45;
+var money2 = 55;
+var 3money = 55;
+
+// 5. name is case sensitive 
+address
+Address 
+ADDRESS
+addRESS
+ADDress 
+
+// 6. how to write a long variable name
+
+var my current home address = "AndorKillla Bandorbon";
+var mycurrenthomeaddress = "AndorKillla Bandorbon";
+var my_current_home_address = "AndorKillla Bandorbon"; // snake case 
+var myCurrentHomeAddress = "AndorKillla Bandorbon"; //Camle Case
+var MyCurrentHomeAddress = "AndorKillla Bandorbon"; //Pascal Case
